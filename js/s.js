b76ae053e45ce20ec74f06df1b8080488fc06156
Marcio Barrios <marciobarrios@gmail.com>
2014-09-03 03:16:09 +0200

    Added serif font and form interaction

--- js/s.js
+++ js/s.js
@@ -14,18 +14,38 @@ if (!supportsSVG()) {
 
 
 /**
- * Mandrill API to send mails
- *
+ * Send mail through the contact form
  */
 (function(w, $, undefined){
   $(".contact-form").on("submit", function(event){
     event.preventDefault();
 
-    var name = $("[name=name]").val(),
-        from = $("[name=email]").val(),
-        time = "<em>" + $("[name=timeline]").val() + "</em>",
-        text = "<p>" + $("[name=description]").val() + "</p>";
+    var $this     = $(this),
+        $formElem = $this.find(":input"),
+        $bt       = $this.find("button"),
+        w         = $bt.width(),
+        h         = $bt.height(),
+        $btText   = $bt.find("span"),
+        btText    = $btText.text(),
+        $spinner  = $bt.find(".spinner"),
+        name      = $("[name=name]").val(),
+        from      = $("[name=email]").val(),
+        time      = "<em>" + $("[name=timeline]").val() + "</em>",
+        text      = "<p>" + $("[name=description]").val() + "</p>";
 
+    // Disabled form elements
+    $formElem.attr("disabled", "disabled");
+
+    // Add px dimensions to button
+    $bt.width(w).height(h);
+
+    // Save current text and hide it
+    $btText.attr("data-text", btText).fadeOut();
+
+    // Show spinner
+    $spinner.fadeIn();
+
+    // Ajax request to Mandrill API
     $.ajax({
       type: "POST",
       url: "https://mandrillapp.com/api/1.0/messages/send.json",
@@ -35,44 +55,31 @@ if (!supportsSVG()) {
           "from_email": from,
           "from_name": name,
           "to": [
-              {
-                "email": "adrian@kendostudio.com",
-                "name": "Kendo Studio",
-                "type": "to"
-              }
-            ],
+            {
+              "email": "adrian@kendostudio.com",
+              "name": "Kendo Studio",
+              "type": "to"
+            }
+          ],
           "autotext": "true",
           "subject": "Contact from kendo.com",
           "html": time + text
         }
       }
-     }).done(function(response) {
-       console.log(response);
-     });
+     })
+    .done(function(response) {
+      // On success, hide the spinner and show a message
+      $spinner.fadeOut();
+      $btText
+        .text("Message sent")
+        .fadeIn()
+        .delay(2000)
+        .fadeOut(function(){
+          $(this).text(btText).fadeIn();
+
+          // Clean and enable form elements
+          $formElem.removeAttr("disabled").not("select").val("");
+        });
+    });
   });
-})(window, jQuery);
-
-
-
-
-
-/**
- * Dribbble API call to get Kendo shots
- *
- */
-
-(function(w, $, undefined){
-  $.ajax({
-    type: 'GET',
-    url: 'https://api.dribbble.com/players/kendo/shots/',
-    dataType: 'jsonp',
-    success: function (data) {
-      if (typeof (data) === 'undefined') {
-        // show sample image?
-      }
-      else {
-        console.log(data);
-      }
-    }
-});
 })(window, jQuery);
